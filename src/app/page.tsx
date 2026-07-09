381b28d4c9800ac50f2b574f76e34e0b3835c901
HimawanRakha <himawanrakha11@gmail.com>
2026-07-09 13:10:55 +0700

    temporal testimoni

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
         <Departemen />
         <Prestasi />
         <Galeri />
-        <Testimoni />
+        {/* <Testimoni /> */}
         <Kontak />
       </main>
       <Footer />
